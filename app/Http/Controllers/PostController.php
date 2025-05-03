e13f2aeaa9363cb73c19b979d87f0698a9f162be
Mejdi Alaoui <mejdi.alaoui-smaili@etu.univ-lyon1.fr>
2025-05-03 18:36:53 +0200

    Ajout du pouuulett

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
 
         ]);
 
-        return redirect()->back()->with('success', 'Post created successfully !');
+        return redirect()->back()->with('success', 'Post created successfully.');
     }
 
     public function index()
